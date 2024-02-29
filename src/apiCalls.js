10c3a019b985614f22262fe01ae6410ffb59b2a2
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-02-28 19:44:06 -0700

    Fetch user, trip and destination data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,1 +1,23 @@
 //fetch one users data, url will be the main url + current user 
+
+function runGet(url) {
+    let gets = [
+        fetch(url),
+        fetch("http://localhost:3001/api/v1/trips"),
+        fetch("http://localhost:3001/api/v1/destinations"),
+    ]
+
+    return gets
+}
+
+
+function fetchData(url) {
+    return Promise.all(runGet(url))
+    .then(res => {
+        return Promise.all(res.map(item => {
+            return item.json()
+        }))
+    })   
+}
+
+export { fetchData };
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,5 @@
+// IMPORTS
+import { fetchData } from './apiCalls';
 
 
 
@@ -7,6 +9,35 @@ let welcome = document.querySelector("#welcome-div")
 
 
 // GLOBAL VARIABLES 
-let currentUser = "1"
-let currentURL = "http://localhost:3001/api/v1/travelers/" + currentUser
+let currentUserID = "1"
+let currentURL = "http://localhost:3001/api/v1/travelers/" + currentUserID
 
+
+
+// DOM UPDATE FUNCTIONS
+
+function renderDom() {
+    fetchData(currentURL)
+    .then(([userInfo, trips, destinations]) => {
+        console.log("userInfo:", userInfo)
+        console.log("trips:", trips)
+        console.log("destinations:", destinations)
+    })
+}
+
+renderDom()
+
+
+
+// fetchData(currentURL)
+// .then(data => console.log("USER INFO:", data))
+
+// fetch("http://localhost:3001/api/v1/trips")
+// .then(res => res.json())
+// .then(data => console.log('TRIPS:', data))
+
+// fetch("http://localhost:3001/api/v1/destinations")
+// .then(res => res.json())
+// .then(data => console.log('DESTINATIONS:', data))
+
+//I want to get single user data, then i want to get trips, then I want to get destinations
