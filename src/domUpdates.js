150248ba01b6fbad08ac2572269bd79a419cc581
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-03-05 01:54:53 -0700

    Add semicolons where appropriate

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,52 +4,52 @@ import { calculateTotalSpent, calculateTripCost, reformatDate, reformatDestinati
 import { imageURLs } from './images';
 
 // QUERY SELECTORS 
-const container = document.querySelector(".container")
-const errorMessage = document.querySelector("#error-message")
-const loginPage = document.querySelector("#login-page")
-const loginButton = document.querySelector("#login-button")
-const usernameField = document.querySelector("#username-field")
-const passwordField = document.querySelector("#password-field")
-const incorrectMessage = document.querySelector("#incorrect-login")
-const header = document.querySelector("header")
-const welcome = document.querySelector("#welcome-div")
-const domName = document.querySelector("#name")
-const pastTripsWidget = document.querySelector("#past-trips")
-const pendingTripsWidget = document.querySelector("#pending-trips")
-const noPastTrips = document.querySelector(".no-past-trips")
-const noPendingTrips = document.querySelector(".no-pending-trips")
-const pastGrid = document.querySelector(".past-grid")
-const pendingGrid = document.querySelector(".pending-grid")
-const totalSpent = document.querySelector("#total-spent")
-const bookNowButton = document.querySelector(".book-now-button")
-const bookingForm = document.querySelector(".booking-form")
-const pastBookButton = document.querySelector("#past-book-button")
-const pendingBookButton = document.querySelector("#pending-book-button")
-const costEstimateDiv = document.querySelector(".cost-estimate-div")
-const costEstimate = document.querySelector("#cost-estimate")
-const costButton = document.querySelector("#see-cost-button")
-const destinationField = document.querySelector("#destination-field")
-const dateField = document.querySelector("#date-field")
-const travelersField = document.querySelector("#travelers-field")
-const durationField = document.querySelector("#duration-field")
-const submitBookingButton = document.querySelector("#submit-booking-button")
-const bottom = document.querySelector("#bottom-div")
+const container = document.querySelector(".container");
+const errorMessage = document.querySelector("#error-message");
+const loginPage = document.querySelector("#login-page");
+const loginButton = document.querySelector("#login-button");
+const usernameField = document.querySelector("#username-field");
+const passwordField = document.querySelector("#password-field");
+const incorrectMessage = document.querySelector("#incorrect-login");
+const header = document.querySelector("header");
+const welcome = document.querySelector("#welcome-div");
+const domName = document.querySelector("#name");
+const pastTripsWidget = document.querySelector("#past-trips");
+const pendingTripsWidget = document.querySelector("#pending-trips");
+const noPastTrips = document.querySelector(".no-past-trips");
+const noPendingTrips = document.querySelector(".no-pending-trips");
+const pastGrid = document.querySelector(".past-grid");
+const pendingGrid = document.querySelector(".pending-grid");
+const totalSpent = document.querySelector("#total-spent");
+const bookNowButton = document.querySelector(".book-now-button");
+const bookingForm = document.querySelector(".booking-form");
+const pastBookButton = document.querySelector("#past-book-button");
+const pendingBookButton = document.querySelector("#pending-book-button");
+const costEstimateDiv = document.querySelector(".cost-estimate-div");
+const costEstimate = document.querySelector("#cost-estimate");
+const costButton = document.querySelector("#see-cost-button");
+const destinationField = document.querySelector("#destination-field");
+const dateField = document.querySelector("#date-field");
+const travelersField = document.querySelector("#travelers-field");
+const durationField = document.querySelector("#duration-field");
+const submitBookingButton = document.querySelector("#submit-booking-button");
+const bottom = document.querySelector("#bottom-div");
 
 // GLOBAL VARIABLES 
 let currentUserID;
 let currentURL;
-let tripID = Date.now() 
+let tripID = Date.now(); 
 let currentIndex = 0;
 
 // EVENT LISTENERS
-loginButton.addEventListener("click", detectLogin)
-bookNowButton.addEventListener("click", displayForm)
-pastBookButton.addEventListener("click", displayForm)
-pendingBookButton.addEventListener("click", displayForm)
+loginButton.addEventListener("click", detectLogin);
+bookNowButton.addEventListener("click", displayForm);
+pastBookButton.addEventListener("click", displayForm);
+pendingBookButton.addEventListener("click", displayForm);
 costButton.addEventListener("click", function(event) {
     event.preventDefault(); 
     displayCost()
-})
+});
 submitBookingButton.addEventListener("click", function(event) {
     event.preventDefault();
     noPendingTrips.classList.add("hidden")
@@ -66,21 +66,21 @@ submitBookingButton.addEventListener("click", function(event) {
     .catch(err => {
         displayErrorMessage(err)
     })
-})
+});
 
 // DOM UPDATE FUNCTIONS
 function renderDom() {
-    destinationField.value = ""
+    destinationField.value = "";
     fetchData(currentURL)
     .then(([userInfo, trips, destinations]) => {
-        let id = parseInt(currentUserID)
-        displayName(userInfo)
-        displayPastTrips(id, trips, destinations)
-        displayPendingTrips(id, trips, destinations)
-        displayTotalSpent(id, trips, destinations)
+        let id = parseInt(currentUserID);
+        displayName(userInfo);
+        displayPastTrips(id, trips, destinations);
+        displayPendingTrips(id, trips, destinations);
+        displayTotalSpent(id, trips, destinations);
     })
     .catch((err) => {
-        displayErrorMessage(err)
+        displayErrorMessage(err);
     })
 }
 
@@ -91,37 +91,37 @@ function detectLogin(event) {
     let secondHalf = array.slice(8).join("")
 
     if (passwordField.value === "traveler" && firstHalf === "traveler" && parseInt(secondHalf) > 0 && parseInt(secondHalf) <= 50 && secondHalf.length <= 2) {
-        loginPage.style.display = "none"
-        header.classList.remove("hidden")
-        container.classList.remove("hidden")
-        bottom.classList.remove("hidden")
-        currentUserID = secondHalf
-        currentURL = "http://localhost:3001/api/v1/travelers/" + currentUserID
+        loginPage.style.display = "none";
+        header.classList.remove("hidden");
+        container.classList.remove("hidden");
+        bottom.classList.remove("hidden");
+        currentUserID = secondHalf;
+        currentURL = "http://localhost:3001/api/v1/travelers/" + currentUserID;
         incorrectMessage.innerText = "";
         usernameField.value = "";
         passwordField.value = "";
         renderDom();
     } else {
-        incorrectMessage.innerText = `I'm sorry, we do not have record of that username and password combination. Please try again.`
+        incorrectMessage.innerText = `I'm sorry, we do not have record of that username and password combination. Please try again.`;
         usernameField.value = "";
         passwordField.value = "";
     }
 }
 
 function displayForm() {
-    container.classList.toggle("hidden")
-    bookingForm.classList.toggle("hidden")
+    container.classList.toggle("hidden");
+    bookingForm.classList.toggle("hidden");
 
     if (bookNowButton.innerText === "Book Your Next Trip Now!") {
-        bookNowButton.innerText = "Back Home" 
+        bookNowButton.innerText = "Back Home"; 
     } else {
-        bookNowButton.innerText = "Book Your Next Trip Now!"
+        bookNowButton.innerText = "Book Your Next Trip Now!";
         destinationField.value = "";
         dateField.value = "";
         travelersField.value = "";
         durationField.value = "";
         costEstimate.innerText = "";
-        costEstimateDiv.classList.add("hidden")
+        costEstimateDiv.classList.add("hidden");
     }
 }
 
@@ -151,27 +151,27 @@ function displayName({name}) {
 }
 
 function displayTotalSpent(id, {trips}, {destinations}) {
-    let total = calculateTotalSpent(id, {trips}, {destinations})
+    let total = calculateTotalSpent(id, {trips}, {destinations});
     if (typeof total === "number") {
-        totalSpent.innerHTML = `<p>You have spent a total of $${calculateTotalSpent(id, {trips}, {destinations})} with <span>TravelTracker</span> this year. Users save an average of 18% when booking with <span>TravelTracker.</span></p>`
+        totalSpent.innerHTML = `<p>You have spent a total of $${calculateTotalSpent(id, {trips}, {destinations})} with <span>TravelTracker</span> this year. Users save an average of 18% when booking with <span>TravelTracker.</span></p>`;
     } else {
-        totalSpent.innerHTML = calculateTotalSpent(id, {trips}, {destinations})
+        totalSpent.innerHTML = calculateTotalSpent(id, {trips}, {destinations});
     }
 }
 
 function displayPastTrips(id, {trips}, {destinations}) {
     const pastTrips = trips.filter((trip) => {
-        return trip.userID === id && trip.status === "approved"
+        return trip.userID === id && trip.status === "approved";
     })
 
     if (pastTrips.length) {
         pastTrips.forEach((trip) => {
             trip.destinationName = destinations.find(destination => {
-                return destination.id === trip.destinationID
-            }).destination
+                return destination.id === trip.destinationID;
+            }).destination;
             trip.image = destinations.find(destination => {
-                return destination.id === trip.destinationID
-            }).image
+                return destination.id === trip.destinationID;
+            }).image;
         })
     
         pastTrips.forEach((trip) => {
@@ -180,26 +180,26 @@ function displayPastTrips(id, {trips}, {destinations}) {
                                         <img class="trip-image" src=${trip.image} alt="picture of ${trip.destinationName}">
                                         <p>${reformatDate(trip.date)}</p>
                                         <p>Party of ${trip.travelers}</p
-                                    </div>`
+                                    </div>`;
         })
     } else {
-        noPastTrips.classList.remove('hidden')
+        noPastTrips.classList.remove('hidden');
     }
 }
 
 function displayPendingTrips(id, {trips}, {destinations}) {
     const pendingTrips = trips.filter((trip) => {
-        return trip.status === "pending" && parseInt(trip.userID) === id
+        return trip.status === "pending" && parseInt(trip.userID) === id;
     })
 
     if (pendingTrips.length) {
         pendingTrips.forEach((trip) => {
             trip.destinationName = destinations.find(destination => {
-                return destination.id === trip.destinationID
-            }).destination
+                return destination.id === trip.destinationID;
+            }).destination;
             trip.image = destinations.find(destination => {
-                return destination.id === trip.destinationID
-            }).image
+                return destination.id === trip.destinationID;
+            }).image;
         })
     
         pendingTrips.forEach((trip) => {
@@ -208,28 +208,28 @@ function displayPendingTrips(id, {trips}, {destinations}) {
                                         <img class="trip-image" src=${trip.image} alt="picture of ${trip.destinationName}">
                                         <p>${reformatDate(trip.date)}</p>
                                         <p>Party of ${trip.travelers}</p
-                                    </div>`
+                                    </div>`;
         })
     } else {
-        noPendingTrips.classList.remove('hidden')
+        noPendingTrips.classList.remove('hidden');
     }
 }
 
 function displayErrorMessage(error) {
-    container.classList.add("hidden")
-    loginPage.classList.add("hidden")
-    bookingForm.classList.add("hidden")
-    header.classList.add("hidden")
-    errorMessage.innerText = `${error}. Please check that your server is running properly.`
-    errorMessage.classList.remove("hidden")
+    container.classList.add("hidden");
+    loginPage.classList.add("hidden");
+    bookingForm.classList.add("hidden");
+    header.classList.add("hidden");
+    errorMessage.innerText = `${error}. Please check that your server is running properly.`;
+    errorMessage.classList.remove("hidden");
 }
 
 function changeBackgroundImage() {
-    document.body.style.backgroundImage = `url('${imageURLs[currentIndex]}')`
+    document.body.style.backgroundImage = `url('${imageURLs[currentIndex]}')`;
     currentIndex = (currentIndex + 1) % imageURLs.length;
 }
 
-changeBackgroundImage()
+changeBackgroundImage();
 setInterval(changeBackgroundImage, 15000);
 
 export { displayErrorMessage };
